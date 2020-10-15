e2695adf43ca2fc0676705aff748bde6e5e2aa1c
Judith de la Fuente <jud.yureawen@gmail.com>
2020-10-15 08:23:44 +0200

    add a portal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
+import {createPortal} from 'react-dom'
 import './App.css';
 
+
+const Modal = ({ children, onClose, open }) =>
+  open ?
+    createPortal(
+      <div className='modal'>
+        <button onClick={onClose} className='modal__close'>&times;</button>
+        {children}
+      </div>,
+    document.body)
+    : null
+
 function App() {
+  const [open, setOpen] = useState(false)
+
   return (
-    <div className="App">
+    <div id="app" className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button >Another button</button>
+        <button onClick={() => setOpen(!open)}>Show Modal?</button>
+        <Modal open={open} onClose={() => setOpen(false)}>
+          Test Modal
+        </Modal>
       </header>
     </div>
   );
